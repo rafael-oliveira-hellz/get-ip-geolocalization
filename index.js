ddd18201578cbeaac51d551e7ff87bf49ad662de
Rafael de Oliveira (Hellz) <rafael.oliveirasylva@gmail.com>
2022-11-19 22:21:39 -0300

    setup package.json

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(json({ extended: false }));
 
-app.use("/api/ip-geolocalization", IpGeolocation);
+app.use("/api", IpGeolocation);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
